774448a179ceb4bc62370e596f4ae0c78b7c3633
Zakkaray <bloomzakk@gmail.com>
2020-07-02 22:25:00 +0300

    template files deleted

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./Navbar";
-import { Props } from "../interfaces/layout";
+import Navbar from "../Navbar";
+import { Props } from "../../interfaces/layout";
 
 const Layout: React.FC<Props> = ({ children }) => (
   <>
